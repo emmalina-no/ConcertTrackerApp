b39a23beded2d53075aff031eb38cd244623717d
Emma Lina Bitar <ELB@pureservice.com>
2026-08-28 10:34:37 +0200

    style: tab bar colors

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,9 +1,23 @@
+import { useTheme } from "@/hooks/use-theme";
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
+	const theme = useTheme();
+
 	return (
-		<Tabs screenOptions={{ headerShown: false }}>
+		<Tabs
+			screenOptions={{
+				headerShown: false,
+				tabBarStyle: {
+					backgroundColor: theme.backgroundElement,
+					borderTopColor: theme.backgroundSelected,
+					// borderTopWidth: 0,
+				},
+				tabBarActiveTintColor: theme.accentWarm,
+				tabBarInactiveTintColor: theme.textSecondary,
+			}}
+		>
 			<Tabs.Screen
 				name="index"
 				options={{
